4bdcf6146da8d9f2d18a519c2e9b3788dd2df4af
KhanhNV <KhanhNV@fpts.com.vn>
2025-11-20 14:33:53 +0700

    update code mddsSaverRedis

---
+++ HnxMddsSaverRD/BackgroundTasks/HnxRedisConsumerWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading;
+using System.Threading.Tasks;
+using MddsSaver.Application.Shared;
+using MddsSaver.Core.Shared.Models;
+using MddsSaver.Core.Shared.Interfaces;
+using MddsSaver.Application.Shared.Interfaces;
+
+namespace HnxMddsSaverRD.BackgroundTasks
+{
+    public class HnxRedisConsumerWorker : BaseMessageConsumerWorker<HnxRedisConsumerWorker>
+    {
+        public HnxRedisConsumerWorker(
+            IServiceScopeFactory scopeFactory,
+            ILogger<HnxRedisConsumerWorker> logger,
+            AppSetting appsetting,
+            IMessageParserFactory parserFactory,
+            IServiceProvider serviceProvider,
+            IHnxRedisDataSaver dataSaver,
+            IMessageTypeFilter msgFilter,
+            IMonitor monitor
+            ) : base(
+                scopeFactory,
+                logger,
+                appsetting,
+                parserFactory,
+                serviceProvider.GetServices<Channel<object>>().First(),
+                dataSaver,
+                msgFilter,
+                monitor
+                )
+        {
+
+        }
+        protected override string GetSourceIdentifier() => "Redis_Hnx";
+    }
+}
---
+++ HnxMddsSaverRD/RedisMessageTypeFilter.cs
@@ -0,0 +1,16 @@
+﻿using MddsSaver.Core.Shared.Entities;
+using MddsSaver.Core.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HnxMddsSaverRD
+{
+    public sealed class RedisMessageTypeFilter : IMessageTypeFilter
+    {
+        private static readonly HashSet<string> _allowed = new() { EPrice.__MSG_TYPE, ESecurityDefinition.__MSG_TYPE };
+        public bool Accept(string msgType) => msgType != null && _allowed.Contains(msgType);
+    }
+}
